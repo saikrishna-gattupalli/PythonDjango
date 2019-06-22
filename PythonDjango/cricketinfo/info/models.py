1ca099ad6f3b113a083c9239d5c06a230334499b
j.rambabu1993@gmail.com <j.rambabu1993@gmail.com>
2019-06-22 10:56:54 +0530

    first django commit

--- PythonDjango/cricketinfo/info/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Country(models.Model):
-	name = models.CharField(max_length = 250, unique=True)
-	flag = models.ImageField(blank=True)
-	shortname = models.CharField(max_length=250, unique=True)
-	description = models.TextField(max_length=500,blank=True, null=True)
